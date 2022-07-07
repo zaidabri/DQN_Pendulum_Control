cee9c4f5226cdb56273334c117ce024893dde701
zaidabri <83652632+zaidabri@users.noreply.github.com>
2022-07-07 23:20:34 +0200

    Update __init__.py

--- pendulum/__init__.py
+++ pendulum/__init__.py
@@ -2,5 +2,5 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from .display import Display
-from .pendulum import Pendulum
+from .pendulum import Pendulums
 from .pend_Hybrid import Single_Hpendulum
